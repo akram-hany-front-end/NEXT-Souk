0f8aa57dc2e528a3eeffe7e84d5508a331435625
akram-hany-front-end <akramhanyahmed@gmail.com>
2026-08-15 17:34:53 +0300

    test5

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 
-import connectDB from "@/lib/mongodb";
 import User from "@/models/User";
+import { connectDB } from "@/lib/mongoose";
 
 export async function POST(request: Request) {
     try {
